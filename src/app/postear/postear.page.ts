a20a4cb1fae0cfbac25d8a5829f2b4f5ed709d02
Marcelo2500 <51750852+Marcelo2500@users.noreply.github.com>
2021-11-16 16:29:34 -0300

    Actualizacion 2

---
+++ src/app/postear/postear.page.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud.service';
+// permite devolver al usuario a la pagina playas(listado)
+import { Router  } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+@Component({
+  selector: 'app-postear',
+  templateUrl: './postear.page.html',
+  styleUrls: ['./postear.page.scss'],
+})
+export class PostearPage implements OnInit {
+
+
+
+  constructor(private toast: ToastController,
+              private crud: CrudService,
+              private router  : Router) { }
+
+  ngOnInit() {
+  }
+
+  async agregar(txtTitulo : HTMLInputElement, 
+          txtUrl: HTMLInputElement, 
+          txtComentario: HTMLInputElement)
+  {
+
+  if(txtTitulo.value.trim().length == 0)
+  {
+    const toast = await this.toast.create({
+      message : 'No ha ingresado un titulo',
+      duration: 2000,
+      color   : "danger",
+      position: "middle"
+    });
+    toast.present();
+  }
+  else if(txtUrl.value.trim().length == 0)
+  {
+    const toast = await this.toast.create({
+      message : 'No ha ingresado una imagen',
+      duration: 2000,
+      color   : "danger",
+      position: "middle"
+    });
+    toast.present();
+  }
+  else if(txtComentario.value.trim().length == 0)
+  {
+    const toast = await this.toast.create({
+      message : 'No ha ingresado un comentario',
+      duration: 2000,
+      color   : "danger",
+      position: "middle"
+    });
+    toast.present();
+  }
+  const post =  [{"isUs": ,
+                  "titulo": txtTitulo.value,
+                  "url"  : txtUrl.value,
+                  "comentario"  : txtComentario.value,
+                    }]
+  this.crud.addPost(post);
+
+  // envia al usuario a otra pagina
+  this.router.navigate(['/playas']);
+  }
+  
+
+}
